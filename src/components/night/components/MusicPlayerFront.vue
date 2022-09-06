86cfc3490fd15fa7eb6f529ce480c39d20b012da
Hanna Jang <letnumdot124@gmail.com>
2022-09-06 23:49:25 +0900

    🪐[UPDATE] buttons  in music player front

--- src/components/night/components/MusicPlayerFront.vue
+++ src/components/night/components/MusicPlayerFront.vue
@@ -10,7 +10,7 @@
         </div>
       </div>
       <div class="playing">
-        <span>PLAYING NOW</span>
+        <span>NOW PLAYING</span>
       </div>
       <div class="menu">
         <div class="circle"></div>
@@ -22,28 +22,56 @@
       </div>
     </div>
     <div class="album-cover">
-      
+      <div class="circle"></div>
+      <div class="circle-shadow"></div>
+      <div class="inner-album">
+        <img class="me" src="@/assets/images/sticker.jpg" alt="sticker">
+      </div>     
     </div>
     <div class="music-content">
       <div class="title">
-
+        Sticker
       </div>
       <div class="singer">
-
+        NCT 127
       </div>
-      <div class="playing-bar">
-
+    </div>
+    <div class="playing-bar">
+      <div class="time">
+        <span class="now-time">00:00</span>
+        <span class="end-time">03:47</span>
+      </div>
+      <div class="bar">
+        <div class="playing-length"></div>
+      </div>
+      <div class="now-playing">
+        <div class="now"></div>
+        <div class="now-shadow"></div>
+        <div class="now-inner"></div>
       </div>
     </div>
     <div class="player-buttons">
       <div class="previous">
-
+        <div class="circle"></div>
+        <div class="circle-shadow"></div>
+        <div class="button-icon">
+          <i class="fas fa-backward"></i>
+        </div>
       </div>
       <div class="play-pause">
-
+        <div class="circle"></div>
+        <div class="circle-shadow"></div>
+        <div class="inner-button"></div>
+        <div class="button-icon">
+          <i class="fas fa-pause"></i>
+        </div>
       </div>
       <div class="next">
-
+        <div class="circle"></div>
+        <div class="circle-shadow"></div>
+        <div class="button-icon">
+          <i class="fas fa-forward"></i>
+        </div>
       </div>
     </div>
   </div>
